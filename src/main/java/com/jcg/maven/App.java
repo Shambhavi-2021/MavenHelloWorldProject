c6bd0fb400e23056fb3cba5ca2b82ab616d66648
Shambhavi-2021 <83941972+Shambhavi-2021@users.noreply.github.com>
2021-08-25 13:02:53 +0530

    Update App.java

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "My first maven build1!" );
+        System.out.println( "My first maven build2!" );
     }
 }
